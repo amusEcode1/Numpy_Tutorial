19ad5074142a54cedd8a1b9a80dd539af5ef574a
Ezekiel Oluyale <amusecode@gmail.com>
2025-03-18 06:53:47 -0700

    Add files via upload

---
+++ numpy_array_dimensions.py
@@ -0,0 +1,24 @@
+import numpy as np
+#0 Dimensional Array
+arr_0D = np.array(67)
+#1 Dimensional Array
+arr_1D = np.array([3,7,4,9])
+#2 Dimensional Array
+arr_2D = np.array([[5,2,4],[3,9,7]])
+#3 Dimensional Array
+arr_3D = np.array([[[4,7,8],[6,2,1]],[[1,2,3],[7,3,5]]])
+#Print out each arrays
+print('0D Array: ', arr_0D)
+print('1D Array: ', arr_1D)
+print('2D Array: ', arr_2D)
+print('3D Array: ', arr_3D)
+#To check the number of dimensions => ndim
+print(f'{arr_0D.ndim} Dimension')
+print(f'{arr_1D.ndim} Dimension')
+print(f'{arr_2D.ndim} Dimension')
+print(f'{arr_3D.ndim} Dimension')
+#Higher Dimensional Array
+arr = np.array([6,6,7,8,3],ndmin=10)
+print('Number of Dimension: ', arr)
+print('OR')
+print(f'Number of Dimension: {arr}')
