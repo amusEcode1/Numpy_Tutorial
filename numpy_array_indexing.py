ef7deea790d3724f74d37246b9efded258d3ec39
Ezekiel Oluyale <amusecode@gmail.com>
2025-03-21 15:48:08 -0700

    Update numpy_array_indexing.py

--- numpy_array_indexing.py
+++ numpy_array_indexing.py
@@ -1,37 +1,37 @@
 import numpy as np
 #1-D Array Indexing
 print('Array Indexing of 1-D Array')
-arr_1D = np.array([7,3,5,1])
-print(arr_1D[0])
-print(f'Sum of the 2nd and the 4th Element: {arr_1D[1] + arr_1D[3]} \n')
+arr = np.array([7,3,5,1])
+print(arr[0])
+print(f'Sum of the 2nd and the 4th Element: {arr[1] + arr[3]} \n')
 
 #2-D Array Indexing
 print('Array Indexing of 2-D Array')
-arr_2D = np.array([[4,6,2],[9,3,5]])
-print(f'3rd Element on 1st row: {arr_2D[0 , 2]}')
-print(f'{arr_2D[0 , 2] + arr_2D[1 , 1]} \n')
+arr = np.array([[4,6,2],[9,3,5]])
+print(f'3rd Element on 1st row: {arr[0 , 2]}')
+print(f'{arr[0 , 2] + arr[1 , 1]} \n')
 
 #3-D Array Indexing
 print('Array Indexing of 3-D Array')
-arr_3D = np.array([[[3,6,7,8],[5,2,3,4]],[[5,2,3,4],[7,1,2,3]],[[1,2,3,4],[2,3,4,5]],[[6,7,8,7],[6,2,3,4]]])
-print(f'3rd Element on 2nd row in the 1st depth: {arr_3D[0, 1, 2]}')
-print(f'{arr_3D[0 , 1, 3] + arr_3D[3 , 0, 2]}\n')
+arr = np.array([[[3,6,7,8],[5,2,3,4]],[[5,2,3,4],[7,1,2,3]],[[1,2,3,4],[2,3,4,5]],[[6,7,8,7],[6,2,3,4]]])
+print(f'3rd Element on 2nd row in the 1st depth: {arr[0, 1, 2]}')
+print(f'{arr[0 , 1, 3] + arr[3 , 0, 2]}\n')
 
 #Negative Indexing: To access an Array/Element from the end
 print('Negative Indexing')
 #1-D Negative Array Indexing
-arr_1D_negArr = np.array([7,3,5,1])
-a = arr_1D_negArr[-1]
-b = arr_1D_negArr[-3]
+arr = np.array([7,3,5,1])
+a = arr[-1]
+b = arr[-3]
 print(a, b)
 print(f'{a + b} \n')
 
 #2-D Negative Array Indexing
-arr_2D_negArr = np.array([[4,6,2],[9,3,5]])
-print(arr_2D_negArr[0, -1])
-print(arr_2D_negArr[-1, 0])
-print(f'The sum of the 2 gives {arr_2D_negArr[0, -1] + arr_2D_negArr[-1, 0]}\n')
+arr = np.array([[4,6,2],[9,3,5]])
+print(arr[0, -1])
+print(arr[-1, 0])
+print(f'The sum of the 2 gives {arr[0, -1] + arr[-1, 0]}\n')
 
 #3-D Negative Array Indexing
-arr_3D_negArr = np.array([[[3,6,7,8],[5,2,3,4]],[[5,2,3,4],[7,1,2,3]]])
-print(arr_3D_negArr[-1,0,3])
+arr = np.array([[[3,6,7,8],[5,2,3,4]],[[5,2,3,4],[7,1,2,3]]])
+print(arr[-1,0,3])
