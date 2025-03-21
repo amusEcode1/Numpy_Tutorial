2314a256c1338b4eb3549858218f01df1aff1d3a
Ezekiel Oluyale <amusecode@gmail.com>
2025-03-21 15:49:17 -0700

    Update numpy_array_dimensions.py

--- numpy_array_dimensions.py
+++ numpy_array_dimensions.py
@@ -1,27 +1,27 @@
 import numpy as np
 #0 Dimensional Array
-arr_0D = np.array(67)
+arr1 = np.array(67)
 
 #1 Dimensional Array
-arr_1D = np.array([3,7,4,9])
+arr2 = np.array([3,7,4,9])
 
 #2 Dimensional Array
-arr_2D = np.array([[5,2,4],[3,9,7]])
+arr3 = np.array([[5,2,4],[3,9,7]])
 
 #3 Dimensional Array
-arr_3D = np.array([[[4,7,8],[6,2,1]],[[1,2,3],[7,3,5]]])
+arr4 = np.array([[[4,7,8],[6,2,1]],[[1,2,3],[7,3,5]]])
 
 #Print out each arrays
-print('0-D Array: ', arr_0D)
-print('1-D Array: ', arr_1D)
-print('2-D Array: ', arr_2D)
-print('3-D Array: ', arr_3D)
+print('0-D Array: ', arr1)
+print('1-D Array: ', arr2)
+print('2-D Array: ', arr3)
+print('3-D Array: ', arr4, '\n')
 
 #To check the number of dimensions => ndim
-print(f'{arr_0D.ndim} Dimension')
-print(f'{arr_1D.ndim} Dimension')
-print(f'{arr_2D.ndim} Dimension')
-print(f'{arr_3D.ndim} Dimension')
+print(f'{arr1.ndim} Dimension')
+print(f'{arr2.ndim} Dimension')
+print(f'{arr3.ndim} Dimension')
+print(f'{arr4.ndim} Dimension\n')
 
 #Higher Dimensional Array => ndmin
 arr = np.array([6,6,7,8,3],ndmin=10)
