8641bb5e49a99eca27bdc2cedec7cc2eccb8d39f
Ezekiel Oluyale <amusecode@gmail.com>
2025-03-31 15:16:15 -0700

    Update 004_seaborn_module.py

--- numpy_random/004_seaborn_module.py
+++ numpy_random/004_seaborn_module.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 print('Plotting a Displot')
 sns.displot([0,1,2,3,4,5,6,7,8,9])
+sns.displot([0,1,2,3,4,5,6,7,8,9], bins = 3) # bins controls the number of bars in a histogram.
 plt.show()
 
 print('\nPlotting a Displot Without the Histogram')
