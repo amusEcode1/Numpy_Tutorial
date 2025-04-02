8f0f454d28464c56edea780ba2631e22f47900dc
Ezekiel Oluyale <amusecode@gmail.com>
2025-04-02 21:32:56 +0100

    Add files via upload

---
+++ numpy_random/009_logistic_distribution.py
@@ -0,0 +1,30 @@
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+print('Logistic Distribution')
+x = random.logistic(loc = 2, scale = 5, size = 20)
+y = random.logistic(loc = 1, scale = 2, size = (3,7))
+print(x)
+print(y)
+
+print('Visualization of Logistic Distribution')
+sns.displot(random.logistic(size = 10), kind = 'kde')
+plt.show()
+
+print('\nDifference between Normal and Logistic Distribution')
+data = {
+    'normal': random.normal(loc = 4, scale = 9, size = 2000),
+    'logistic': random.logistic(size = 1000)
+}
+sns.displot(data, kind = 'kde')
+plt.title('Normal Distribution vs Logistic Distribution')
+plt.show()
+
+print('\nDifference between Poisson and Logistic Distribution')
+data = {
+    'normal': random.poisson(lam = 6, size = 2000),
+    'logistic': random.logistic(size = 1000)
+}
+sns.displot(data, kind = 'kde')
+plt.title('Poisson Distribution vs Logistic Distribution')
+plt.show()
