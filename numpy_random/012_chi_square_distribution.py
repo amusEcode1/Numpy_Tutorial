3d64d25c96f150909db5e0700922dd02c8ded6ac
Ezekiel Oluyale <amusecode@gmail.com>
2025-04-04 20:41:13 +0100

    Add files via upload

--- numpy_random/012_chi_square_distribution.py
+++ numpy_random/012_chi_square_distribution.py
@@ -10,3 +10,11 @@ print(y)
 print('\nVisualization of Chi Square Distribution')
 sns.displot(random.chisquare(df = 1, size = 1000), kind = 'kde')
 plt.show()
+
+print('\nDifference between Exponential and Chi Square Distribution')
+data = {
+    'exponential': random.exponential(size = 200),
+    'chi square': random.chisquare(df= 4, size = 200)
+}
+sns.displot(data, kind = 'kde')
+plt.show()
